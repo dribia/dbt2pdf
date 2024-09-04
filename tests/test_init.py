5de4db7fdad4754fd3b51d1f2ef35f833d19494a
Albert Iribarne <iribarne@dribia.com>
2024-09-04 14:42:18 +0200

    :recycle: Remove legaacy test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,5 @@
 """Test the module initialization and version information."""
 
-import re
 import sys
 from importlib import reload
 from importlib.metadata import PackageNotFoundError
@@ -17,7 +16,6 @@ class TestInit:
         """Test `__version__` when the package is not found."""
         if "dbt2pdf" in sys.modules:
             del sys.modules["dbt2pdf"]
-        import dbt2pdf
 
         reload(dbt2pdf)
         assert dbt2pdf.__version__ == "unknown"
@@ -28,14 +26,6 @@ class TestInit:
         mock_version.return_value = "0.1.0"
         if "dbt2pdf" in sys.modules:
             del sys.modules["dbt2pdf"]
-        import dbt2pdf
 
         reload(dbt2pdf)
         assert dbt2pdf.__version__ == "0.1.0"
-
-    def test_version(self):
-        """Test the actual `__version__` value is valid."""
-        assert (
-            re.match(r"\d.\d.\d", dbt2pdf.__version__)
-            or dbt2pdf.__version__ == "unknown"
-        )
